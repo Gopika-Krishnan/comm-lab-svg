edf1fb7fcf40e1543c63f6e0bcb638cdfb170566
Gopika Krishnan <gk1656@nyu.edu>
2021-10-05 15:12:47 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+let insideofBridge = document.getElementById("Inside");
+insideofBridge.addEventListener("click",() =>{
+ document.getElementById("textbox").innerText = "You've clicked on the inside of the bridge!!"
+})
